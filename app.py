763d3516d0a347a8e562eb22a3b464c7589d9449
Univers_lord <srinishjai@gmail.com>
2024-04-24 14:37:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
+print("""Hi user 
+
+
+this is made by universe_lord to learn python as hes a noob.
+
+
+this is not powerfull to do alot of numbers and cannot do
+
+
+please input the first number""")
 a=int(input())
+print("please input the second number")
 b=int(input())
+print("please input the third number")
 c=int(input())
 d=b-a
 e=c-b
